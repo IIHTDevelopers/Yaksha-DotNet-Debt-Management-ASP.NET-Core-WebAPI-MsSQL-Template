5c340895bd4a7461cf905e0e0d5ddaf724db2686
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-06-06 18:15:38 +0530

    Update DebtController.cs

--- DebtManagement/Controllers/DebtController.cs
+++ DebtManagement/Controllers/DebtController.cs
@@ -19,8 +19,6 @@ namespace DebtManagement.Controllers
             _DebtService = DebtService;
         }
 
-        [HttpPost]
-        [Route("create-debt")]
         [AllowAnonymous]
         public async Task<IActionResult> CreateDebt([FromBody] Debt model)
         {
@@ -29,16 +27,13 @@ namespace DebtManagement.Controllers
         }
 
 
-        [HttpPut]
-        [Route("update-debt")]
         public async Task<IActionResult> UpdateDebt([FromBody] DebtViewModel model)
         {
             //write your code here
             throw new NotImplementedException();
         }
 
-        [HttpDelete]
-        [Route("delete-debt")]
+        
         public async Task<IActionResult> DeleteDebt(int id)
         {
             //write your code here
@@ -46,16 +41,13 @@ namespace DebtManagement.Controllers
         }
 
 
-        [HttpGet]
-        [Route("get-debt-by-id")]
         public async Task<IActionResult> GetDebtById(int id)
         {
             //write your code here
             throw new NotImplementedException();
         }
 
-        [HttpGet]
-        [Route("get-all-debts")]
+
         public async Task<IEnumerable<Debt>> GetAllDebts()
         {
             //write your code here
